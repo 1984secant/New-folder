7ad1fc119daa06779de66160f6dc56c4a0755eb9
1984secant <127aacrooms@frhsd.com>
2025-11-03 12:52:32 -0500

    Hello

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let nameBtn = document.querySelector('#nameBtn');
 
 nameBtn.addEventListener("click", changeName);
 ogName.addEventListener("click", revertName);
+// all of these names are characters that belong to my friends and i
+//dullahan and xethorie are my fellas
 var nameList = ["Onett", "Apollo", "Tavern", "Profes", "Marksman", "Dullahan", "CAC", "Judge", "Lottie", "Nylanthotep", "Emporo", "Jaeger", "Cesare", "Mai", "Clever Jr.", "Rook", "Vitiri", "Xethorie", "Orion", "Jinx", "Lycan", "Caspian", "Priest Zaphkiel", "Jackthaniel" ]
 
 function changeName(){
