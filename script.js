66fe6b27e92650e58a5db69d571cd8093ac10a55
1984secant <127aacrooms@frhsd.com>
2025-11-04 14:01:05 -0500

    update3

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ nameBtn.addEventListener("click", changeName);
 ogName.addEventListener("click", revertName);
 // all of these names are characters that belong to my friends and i
 //dullahan and xethorie are my fellas
-var nameList = ["Onett", "Apollo", "Tavern", "Profes", "Marksman", "Dullahan", "CAC", "Judge", "Lottie", "Nylanthotep", "Emporo", "Jaeger", "Cesare", "Mai", "Clever Jr.", "Rook", "Vitiri", "Xethorie", "Orion", "Jinx", "Lycan", "Caspian", "Priest Zaphkiel", "Jackthaniel" ]
+var nameList = ["Onett", "Apollo", "Tavern", "Profes", "Marksman", "Dullahan", "CAC", "Judge", "Lottie", "Nylanthotep", "Emporo", "Jaeger", "Cesare", "Mai", "Clever Jr.", "Rook", "Vitiri", "Xethorie", "Orion", "Jinx", "Lycan", "Caspian", "Priest Zaphkiel", "Jackthaniel"]
 
 function changeName(){
-    nameTxt.innerHTML = nameList[Math.floor(Math.random()* 25)] ;
+    nameTxt.innerHTML = nameList[Math.floor(Math.random()* 26)] ;
 }
 
 function revertName (){
